7bd104a65e7b4be107a2e7423df7329312471ea1
sadiyabano5157-wq <sadiyabano5157@gmail.com>
2026-05-28 20:19:25 +0530

    Add double butterfly pattern printing program
    This program prints a double butterfly pattern based on the number of rows input by the user.

---
+++ double_butterfly_pattern.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+  
+	printf("Enter a row:");
+	scanf("%d",&n);
+
+	// first butterfly
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+			printf("*");
+
+		for(int j=1;j<=2*(n-i);j++)
+			printf(" ");
+
+		for(int j=1;j<=i;j++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	for(int i=n;i>=1;i--)
+	{
+		for(int j=1;j<=i;j++)
+			printf("*");
+
+		for(int j=1;j<=2*(n-i);j++)
+			printf(" ");
+
+		for(int j=1;j<=i;j++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	// second butterfly
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=i;j++)
+			printf("*");
+
+		for(int j=1;j<=2*(n-i);j++)
+			printf(" ");
+
+		for(int j=1;j<=i;j++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	for(int i=n;i>=1;i--)
+	{
+		for(int j=1;j<=i;j++)
+			printf("*");
+
+		for(int j=1;j<=2*(n-i);j++)
+			printf(" ");
+
+		for(int j=1;j<=i;j++)
+			printf("*");
+
+		printf("\n");
+	}
+
+	return 0;
+}
